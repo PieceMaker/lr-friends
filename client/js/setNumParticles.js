755019b7db453ed613e0acdc4ff2e7f3e942802b
Jonathan Adams <jd.adams16@gmail.com>
2020-05-23 15:11:46 -0500

    Accidentally changed default number of particles.

--- client/js/setNumParticles.js
+++ client/js/setNumParticles.js
@@ -8,7 +8,7 @@
  * @param {number} [particlesPerLetter=40]
  * @returns {void}
  */
-module.exports = function(presets, text, particlesPerLetter = 25) {
+module.exports = function(presets, text, particlesPerLetter = 40) {
     const numLetters = text.replace(/\s/g,'').length;
     presets.particles.number.value = numLetters * particlesPerLetter;
 }
