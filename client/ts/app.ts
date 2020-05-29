859785d6719ace7c85936b810bf0880214d43689
Jonathan Adams <jd.adams16@gmail.com>
2020-05-28 23:36:09 -0500

    Move drawing logic and div creation into separate function.

--- client/ts/app.ts
+++ client/ts/app.ts
@@ -9,6 +9,26 @@ import setNumParticles from "./setNumParticles";
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+const particlesID = 'tsparticles';
+const particleDiv = document.createElement('div');
+particleDiv.setAttribute('id', particlesID);
+
+const renderParticles = function(svg: {width: number, xml: string}) {
+    const existingParticles = document.getElementById(particlesID);
+    if(existingParticles) {
+        // @ts-ignore
+        existingParticles.parentElement.removeChild(existingParticles);
+    }
+    document.body.append(particleDiv);
+
+    fitSVG(svg, presets);
+    // @ts-ignore
+    tsParticles.load(particlesID, presets)
+        .then((container) => {
+            console.log('tsParticles config loaded.');
+        });
+}
+
 const { answer, colors } = getAnswer();
 presets.background.color = colors.background;
 presets.particles.color.value = colors.particles;
@@ -18,11 +38,5 @@ generateSVG(answer.text)
     .then((svg) => {
         // @ts-ignore
         window.fetch = fetchPatch(svg.xml);
-        fitSVG(svg, presets);
-
-        // @ts-ignore
-        tsParticles.load('tsparticles', presets)
-            .then((container) => {
-                console.log('tsParticles config loaded.');
-            });
+        renderParticles(svg);
     });
